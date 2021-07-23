08bc8a46cf3e5e018e2b8b7b86fabace102cb81a
vincentbons <vincent.bons@qbuzz.nl>
2021-07-23 10:41:10 +0200

    Version 1.0.2 Updated numpy and pillow versions.

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup, find_namespace_packages
 
 setup(name='wingedsheep-carcassonne',
-      version='1.0.1',
+      version='1.0.2',
       description='Carcassonne implementation',
       author='Vincent Bons',
       url='https://github.com/wingedsheep/carcassonne',
       download_url='https://github.com/wingedsheep/carcassonne',
       license='MIT',
-      install_requires=['numpy>=1.18.1',
-                        'pillow>=7.1.2'],
+      install_requires=['numpy>=1.21.1',
+                        'pillow>=8.3.1'],
       packages=find_namespace_packages(),
       package_data={"": ["*.png", "*.jpg"]})
